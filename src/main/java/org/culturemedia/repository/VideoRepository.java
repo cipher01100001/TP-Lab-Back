fc168013883fe798d4c99c456356c40b34c60421
sergio <cipher01100001@gmail.com>
2024-10-10 11:25:17 -0500

    Adding video structures

---
+++ src/main/java/org/culturemedia/repository/VideoRepository.java
@@ -0,0 +1,12 @@
+package org.culturemedia.repository;
+
+import org.culturemedia.model.Video;
+
+import java.util.List;
+
+public interface VideoRepository {
+    List<Video> findAll();
+    Video save(Video save);
+    List<Video> find(String title);
+    List<Video> find(Double fromDuration, Double toDuration);
+}
